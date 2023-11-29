4bb3c7d20efbf76806dfef5eb60c4e85e8765327
alexrashkin <r100aa@yandex.ru>
2023-11-29 23:06:06 +0300

    serializers created

---
+++ backend/api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from products.models import Dealer, Product, DealerPrice, ProductDealerKey
+
+class DealerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dealer
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class DealerPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DealerPrice
+        fields = '__all__'
+
+class ProductDealerKeySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductDealerKey
+        fields = '__all__'
