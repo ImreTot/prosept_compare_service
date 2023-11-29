0201a54640e7e338a4dcb3c365e1a978958fb9e3
alexrashkin <r100aa@yandex.ru>
2023-11-29 20:11:08 +0300

    endpoints added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import LoadDataView, MainView
+from .views import LoadDataView, MainView, MatchingOptionsView, MarkupProductView, StatisticsView
 
 urlpatterns = [
     path('load_data/', LoadDataView.as_view(), name='load_data'),
-    path('', MainView.as_view(), name='main_view'),
+    path('main_view/', MainView.as_view(), name='main_view'),
+    path('matching_options/<int:product_id>/', MatchingOptionsView.as_view(), name='matching_options'),
+    path('markup_product/<int:product_id>/', MarkupProductView.as_view(), name='markup_product'),
+    path('statistics/', StatisticsView.as_view(), name='statistics'),
 ]
