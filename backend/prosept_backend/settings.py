6ab4ef15a2972f0efadba05b68e6e9bc090fb00f
saycoron <saycoron@gmail.com>
2023-12-03 13:58:31 +0300

    Correct reuirements.

--- backend/prosept_backend/settings.py
+++ backend/prosept_backend/settings.py
@@ -25,8 +25,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
+
     'api',
     'products',
 ]
